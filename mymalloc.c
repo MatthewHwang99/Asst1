24fa444002ce93c89cfd0d031d64e6badcbb9587
Matthew Hwang <mh1160@ilab2.cs.rutgers.edu>
2019-10-09 23:48:19 -0400

    Edited mymalloc.h, started mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,7 +1,37 @@
 #include "mymalloc.h"
 
-struct node{
-	char c;
-	short i;
-	struct node *next;
+void* mymalloc(int size, char* filename, int lineNum){
+	//check to see if the size is valid
+	if(size < 0 || size > 4096){
+		printf("Error at line %d in file %s:Invalid size.\n", lineNum, fileName);
+	}
+
+	char magic_num = 127; //used to check if mymalloc has been called yet...
+	
+	struct md* temp = (struct md*) myblock; 
+	
+	struct md* newMD;
+	newMD.isFree = '0';
+	newMD.size = size;
+	newMD.next = NULL;
+	
+	if(myblock[0] != magic_num){ //check for mymalloc's first call
+		myblock[0] = magic_num;
+		temp = (char*)temp + 1;
+		struct md* nextMD;
+		nextMD.size = 4096 - (sizeof(struct md)*2 + size + 1); //size of the rest of myblock
+		nextMD.isFree = 1; 
+		nextMD.next = NULL;
+		newMD.next = &nextMD;
+		*temp = newMD;
+		
+		return (void*)( (char*)temp + sizeof(struct md)); //return location where the memory (not the MD) is pointing to
+	}
+	
+	return;
+}
+
+void myfree(int size, char* filename, int lineNum){
+
+	return;
 }
--- mymalloc.h
+++ mymalloc.h
@@ -1,5 +1,31 @@
 #ifndef mymalloc.h
 #define mymalloc.h
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<time.h>
+#include<math.h>
+
+//macros to replace all calls to malloc(x) with calls to mymalloc(x)
+#define malloc(x) mymalloc(x, __FILE__, __LINE__)
+//macros to replace all calls to free(x) with myfree(x)
+#define free(x) myfree(x, __FILE__, __LINE__)
+
+//function signature for mymalloc(x) code
+void* mymalloc(int, __FILE__, __LINE__);
+
+//function signature for myfree(x) code
+void* myfree(int, __FILE__, __LINE__);
+
+//metadata struct definition
+struct md{
+	char isFree; //use a char to store whether the block of memory is "free" | '0' = Not free, '1' = Free
+	short size; //use a short to store the size of the block of memory since it is smaller than int
+	struct md *next; //pointer to the next metadata entry
+}
+
+//definition for a static array of size 4096 to allocate from
+static char myblock[4096];
 
 #endif
