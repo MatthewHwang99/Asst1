30dbedc3c56c3a7ac866e469fb0a0eb449ca79a0
Matthew Hwang <mh1160@ilab2.cs.rutgers.edu>
2019-10-14 20:48:30 -0400

    fixed errors

--- memgrind.c
+++ memgrind.c
@@ -1,1 +1,54 @@
-#
+#include "mymalloc.h"
+
+int main(int argc, char** argv){
+	int i; int j;
+	
+	char* p = (char*)malloc(1);
+	free(p);
+	
+	//A: malloc() 1 byte and immediately free it - do this 150 times
+	/*for(i = 0; i<150; i++){
+		void* p=malloc(1);
+		free(p);
+	}*/
+	
+	/*B: malloc() 1 byte, store the pointer in an array - do this 150 times.
+		Once you've malloced 50 byte chunks, then free the 50 1 byte pointers one by one*/
+	/*void* arr[50];
+	for(j = 0; j<3; j++){
+		for(i = 0; i<50; i++){
+			void* p=malloc(1);
+			arr[i]=p;
+		}
+		for(i = 0; i<50; i++){
+			free(arr[i]);
+		}
+	}*/
+	
+	/*C: Randomly choose between a 1 byte malloc or freeing a 1 byte pointer
+			>do this until you have allocated 50 times
+		Keep track of each operation so that you eventually malloc 50 bytes, in total
+			>if you have already allocated 50 times, disregard the random and just free on each iteration
+		Keep track of each operation so that you eventually free all pointer
+			>don't allow a free if you have no pointers to free*/
+	/*void* arr[50];
+	for(i=0;i<50;i++){
+		
+	
+	}*/
+	
+	
+	/*D: Randomly choose between a randomly-sized malloc of free ing a pointer - do this many times
+		- Keep track of each malloc so that all mallocs do not exceed your total memory capacity
+		- Keep track of each operation so that you eventually malloc 50 times
+		- Keep track of each operation so that you eventually free() all pointers
+		- Choose a random allocated size between 1 and 64 bytes*/
+		
+	
+	/*E,F: Two more works loads of your choosing
+		- Both described in testplan.txt*/
+		
+				
+
+	return 0;
+}
--- mymalloc.c
+++ mymalloc.c
@@ -1,12 +1,12 @@
 #include "mymalloc.h"
 
-int getNextAddr(struct metadata currNode, int loc){
-	return currNode.size+sizeof(struct metadata)+loc;
+int getNextAddr(struct metadata* currNode){
+	return currNode->size+sizeof(struct metadata);
 }
 
-void* mymalloc(int size, char* filename, int lineNum){
+void* mymalloc(int size, char* fileName, int lineNum){
   //check to see if the size is valid
-  if(size < 0 || size > (4096 - sizeof(metadata))){
+  if(size < 0 || size > (4096 - sizeof(struct metadata))){
     printf("Error at line %d in file %s: Invalid size.\n", lineNum, fileName);
   }
   
@@ -15,93 +15,61 @@ void* mymalloc(int size, char* filename, int lineNum){
     //First time mymalloc is run
     struct metadata first = {'y', (4096-sizeof(struct metadata))};
     *ptr = first;
-    ptr = &(ptr+sizeof(struct metadata));
+    //ptr = (ptr+1);
     //double check to make sure this is the correct output; should be a pointer to the new data block
-    return (void*)ptr;
+    //return (void*)ptr;
   }
 
   //Iterate through entire array until a free metadata is found with enough size
-  while(ptr <= myblock[4095]){
+  while((char*)ptr <= &myblock[4095]){
     if(ptr->isFree == 'y'){
       if(ptr->size >= size){
-	//block of mem. is free and is big enough
-	if((ptr->size - size) > sizeof(struct metadata)){
-	  //there is enough room for another block of memory
-	  struct metadata new = {'y', (ptr->size - size - sizeof(metadata))};
-	  *(ptr + size) = new;
-	  ptr->size = size;
-	  ptr->isFree = 'n';
-	  ptr = &myblock[ptr+sizeof(metadata)];
-	}else{
-	  //isn't enough space for another metadata struct and more memory so just return the whole block of memory
-	  ptr->isFree = 'n';
-	  ptr = &myblock[ptr+sizeof(metadata)];
-	}
-	return (void*)ptr;
+		//block of mem. is free and is big enough
+		if((ptr->size - size) > sizeof(struct metadata)){
+		  //there is enough room for another block of memory
+		  struct metadata new = {'y', (ptr->size - size - sizeof(struct metadata))};
+		  *(ptr + size) = new;
+		  ptr->size = size;
+		  ptr->isFree = 'n';
+		  //ptr = &myblock[ptr+sizeof(struct metadata)];
+		}else{
+		  //isn't enough space for another metadata struct and more memory so just return the whole block of memory
+		  ptr->isFree = 'n';
+		  //ptr = &myblock[ptr+sizeof(struct metadata)];
+		}
+		return (void*)(ptr+1);
       }
     }
     //Seg fault???
     //or
     //ptr = &(ptr+sizeof(struct metadata));
-    ptr = &myblock[ptr+sizeof(struct metadata)];
+    ptr = (struct metadata*)&myblock[getNextAddr(ptr)];
   }
   //if you get here we could not process the request  
   return NULL;
 }
 
 
-/*
-void* mymalloc(int size, char* filename, int lineNum){
-	//check to see if the size is valid
-	if(size < 0 || size > 4096){
-		printf("Error at line %d in file %s:Invalid size.\n", lineNum, fileName);
-	}
-
-	char magic_num = 127; //used to check if mymalloc has been called yet...
-	
-	struct md* temp = (struct md*) myblock; 
-	
-	struct md* newMD;
-	newMD.isFree = '0';
-	newMD.size = size;
-	newMD.next = NULL;
-	
-	if(myblock[0] != magic_num){ //check for mymalloc's first call
-		myblock[0] = magic_num;
-		temp = (char*)temp + 1;
-		struct md* nextMD;
-		nextMD.size = 4096 - (sizeof(struct md)*2 + size + 1); //size of the rest of myblock
-		nextMD.isFree = 1; 
-		nextMD.next = NULL;
-		newMD.next = &nextMD;
-		*temp = newMD;
-		
-		return (void*)( (char*)temp + sizeof(struct md)); //return location where the memory (not the MD) is pointing to
-	}
-	
-	return;
-}
-*/
-void myfree(void* ptr, char* filename, int lineNum){
-	if(ptr == NULL){ //no operation is performed is ptr is NULL
+void myfree(void* pointer, char* filename, int lineNum){
+	if(pointer == NULL){ //no operation is performed is ptr is NULL
 		return; 
 	}
 	
-	ptr = ptr - sizeof(struct metadata); //move ptr to the metadata of the data being free'd
+	struct metadata* ptr = pointer - sizeof(struct metadata); //move ptr to the metadata of the data being free'd
 	
 	//A: Free()ing addresses that are not pointers:
-	if( !(ptr > &myblock[0] && ptr <= &myblock[4095]) ){
+	if( !((char*)ptr >= &myblock[0] && (char*)ptr <= &myblock[4095]) ){
 		printf("Error at line %d in file %s: Address is not a pointer.\n", lineNum, filename);
 		return;
 	}
 	
 	//B: Free()ing pointers that were not allocated by malloc():
 	//Check each md block to see if ptr is one of the mem block's that were malloc'd
-	struct metadata* temp = (struct metadata*)(myblock+1);
-	int pos = 1;
+	struct metadata* temp = (struct metadata*)(myblock);
+	int pos = 0;
 	while(pos < 4096){
 		if(temp != ptr){
-			pos = getNextAddr(temp, pos);
+			pos = getNextAddr(temp);
 			temp = (struct metadata*)(myblock+pos);
 		}else{
 			break; //temp == ptr | ptr was allocated by mymalloc
@@ -115,27 +83,27 @@ void myfree(void* ptr, char* filename, int lineNum){
 	
 	//C: Redundant free()ing of the same pointer:
 	struct metadata* mdPtr = (struct metadata*) ptr;
-	if(mdPtr.isFree == '1'){
+	if(mdPtr->isFree == 'y'){
 		printf("Error at line %d in file %s: Pointer was already free'd.\n", lineNum, filename);
 		return;
 	}
 	
 	//Since none of the conditions were met above, we can safely free the pointer
-	mdPtr.isFree = '1';
+	mdPtr->isFree = 'y';
 	
 	//Iterate through the metadatas and combined adjacent blocks of memory that are free
-	struct metadata* temp = (struct metadata*)(myblock+1);
+	temp = (struct metadata*)(myblock);
 	struct metadata* prev = temp;
-	pos = getNextAddr(temp, 1);
-	temp = (struct mmetadata*)(myblock+pos);
+	pos = getNextAddr(temp);
+	temp = (struct metadata*)(myblock+pos);
 	
 	while(pos < 4096){
-		if(temp.isFree == '1' && prev.isFree == '1'){ //adj blocks are free, combine them
-			prev.size += (temp.size+sizeof(struct metadata));
-			pos = getNextAddr(temp, pos);
+		if(temp->isFree == 'y' && prev->isFree == 'y'){ //adj blocks are free, combine them
+			prev->size += (temp->size+sizeof(struct metadata));
+			pos = getNextAddr(temp);
 			temp = (struct metadata*)(myblock+pos); //keep prev where it is in case the md after temp is also free
 		}else{
-			pos = getNextAddr(temp, pos);
+			pos = getNextAddr(temp);
 			prev = temp;
 			temp = (struct metadata*)(myblock+pos);
 		}
